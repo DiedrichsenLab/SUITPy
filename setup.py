c9877dbd2dc8466fe99ab199d4b5d7c58a9ce310
Maedbh King <mking86@uwo.ca>
2021-02-15 17:13:28 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="suit",
+    packages=find_packages(),
+    version="0.1.0",
+    description="",
+    author="",
+    license="MIT",
+)
