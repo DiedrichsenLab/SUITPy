cf3e173eba6e7900a5af3c29deabfce15e8d0dce
Maedbh King <mking86@uwo.ca>
2021-06-03 11:24:09 -0700

    added functionality to fetch atlas/surface/contrasts datasets

--- fetch_datasets.py
+++ fetch_datasets.py
@@ -1,15 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import numpy as np
 import os
-import sys
-import nibabel as nb
-import warnings
+import glob
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-surf_dir = os.path.join(_base_dir, 'surfaces')
-atlas_dir = os.path.join(_base_dir, 'atlasesSUIT')
-func_dir = os.path.join(_base_dir, 'functionalMapsSUIT')
+def atlases():
+    """fetch SUIT atlases (*.nii and *.nii.gz)
 
+    Returns:   
+        dictionary containing keys `img_dir` (fullpath to image dir) 
+        and `images` (list of str)
+    """
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    atlas_dir = os.path.join(base_dir, 'atlasesSUIT')
+    os.chdir(atlas_dir)
+
+    # get niftis
+    fpaths = glob.glob('*.nii')
+    fpaths.extend(glob.glob('*.nii.gz'))
+
+    return {'img_dir': atlas_dir, 'images': fpaths}
+
+def surfaces():
+    """fetch SUIT surfaces (*.label.gii and *.func.gii)
+
+    Returns:   
+        dictionary containing keys `img_dir` (fullpath to image dir) 
+        and `images` (list of str)
+    """
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    surf_dir = os.path.join(base_dir, 'surfaces')
+    os.chdir(surf_dir)
+
+    # get surfacs
+    fpaths = glob.glob('*.gii')
+
+    return {'img_dir': surf_dir, 'images': fpaths}
+
+def contrasts():
+    """fetch SUIT contrasts (*.nii) from MDTB dataset
+
+    Returns:   
+        dictionary containing keys `img_dir` (fullpath to image dir) 
+        and `images` (list of str)
+    """
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    con_dir = os.path.join(base_dir, 'functionalMapsSUIT')
+    os.chdir(con_dir)
     
+    # get surfacs
+    fpaths = glob.glob('*.nii')
+
+    return {'img_dir': con_dir, 'images': fpaths}
