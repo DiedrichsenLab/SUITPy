4fbb3045e6f67fb1152fbc567b1f46ba91ddeeac
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2021-11-20 20:10:56 -0500

    Removed unecessary warning for atlases

--- SUITPy/__init__.py
+++ SUITPy/__init__.py
@@ -2,7 +2,6 @@
 Helper functions to download NeuroImaging datasets
 """
 
-from warnings import warn
 from .atlas import (fetch_king_2019,
                     fetch_buckner_2011,
                     fetch_diedrichsen_2009,
@@ -22,6 +21,3 @@ __all__ = [fetch_king_2019, fetch_buckner_2011, fetch_diedrichsen_2009,
         fetch_ji_2019, fetch_xue_2021, vol_to_surf, make_func_gifti,
         make_label_gifti, get_gifti_column_names, get_gifti_colortable,
         get_gifti_anatomical_struct, plot]
-
-warn("Fetchers from the SUITPy.atlas module will be "
-     "updated in later versions as new atlases become available", FutureWarning)
