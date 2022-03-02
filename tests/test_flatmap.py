2527c1e376f0e7c3b0e9698859a1e932df12432c
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2022-03-01 21:43:59 -0500

    Test 2d-shapes

--- tests/test_flatmap.py
+++ tests/test_flatmap.py
@@ -3,14 +3,34 @@
 Unit test for flatmap unit
 @author: jdiedrichsen
 """
-import unittest 
+import unittest
 import SUITPy.flatmap as flatmap
-import numpy as np 
+import numpy as np
+import plotly.graph_objs as go
 
-def test_flatmap_plot(): 
+def make_shapes():
+    sc=[]
+    sc.append(go.Scatter(x=[0,1,2,0], y=[0,2,0,0],
+            fill="toself",
+            fillcolor='rgba(0,0,255,1)',
+            line = dict(width=0),
+            mode='lines'
+            ))
+    sc.append(go.Scatter(x=[1,2,3,1], y=[2,3,3,2],
+            fill="toself",
+            fillcolor="#000A00",
+            line = dict(width=0),
+            mode='lines'
+            ))
+    fig = go.Figure(sc)
+    fig.show()
+    pass
+
+def test_flatmap_plot():
     ax = flatmap.plot('docs/source/notebooks/Buckner_17Networks.label.gii',overlay_type='label',new_figure=True, colorbar=False,render='plotly')
     pass
-    
+
 
 if __name__ == '__main__':
-    test_flatmap_plot()
+    make_shapes()
+    # test_flatmap_plot()
