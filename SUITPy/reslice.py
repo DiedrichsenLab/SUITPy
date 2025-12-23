4323d6a5f0802035c3fab1ffea85d61c12f70222
WangYaping <86057118+WangYaping19970211@users.noreply.github.com>
2025-12-23 15:16:11 -0500

    mask_thr

--- SUITPy/reslice.py
+++ SUITPy/reslice.py
@@ -17,7 +17,8 @@ def reslice_image(
                 voxelsize = None,
                 imagedim = None,
                 affine = None,
-                replace_nan = True):
+                replace_nan = True,
+                mask_thr = None):
     """Reslices images into atlas space using a deformation map and mask 
 
     Args:
@@ -37,6 +38,8 @@ def reslice_image(
             affine transformation matrix of target image
         replace_nan (bool):
             if true, replaces Nan values with 0
+        mask_thr (float):
+            If given, binarizes the mask at this threshold
             
     Returns:
         image (NIFTI image or list of NIFTI Images )
@@ -70,13 +73,13 @@ def reslice_image(
         for img in source_image:
             if type(img) == str:
                 img = nib.load(img)
-            output_img = reslice_img(img, deformation, mask, interp, imagedim,affine,replace_nan)
+            output_img = reslice_img(img, deformation, mask, interp, imagedim, affine, replace_nan, mask_thr)
             output_list.append(output_img)
         return output_list
     else:
         if type(source_image) == str:
             source_image = nib.load(source_image)
-        output_img = reslice_img(source_image, deformation, mask, interp, imagedim, affine,replace_nan)
+        output_img = reslice_img(source_image, deformation, mask, interp, imagedim, affine, replace_nan, mask_thr)
         return output_img
 
 def reslice_img(img,
@@ -85,7 +88,8 @@ def reslice_img(img,
                 interp,
                 imagedim,
                 affine,
-                replace_nan):
+                replace_nan,
+                mask_thr):
     """
     Resample image
 
@@ -104,6 +108,8 @@ def reslice_img(img,
             Affine transformation matrix of desired target image
         replace_nan (bool):
             if true, replaces Nan values with 0
+        mask_thr (float):
+            If given, binarizes the mask at this threshold
     Returns:
         image (NIFTI image or list of NIFTI Images )
     """
@@ -119,6 +125,8 @@ def reslice_img(img,
     data = ntv.sample_image(img, xm, ym, zm, interp)
     if mask != None:
         maskData = ntv.sample_image(mask, xm, ym, zm, interp)
+        if mask_thr is not None:
+            maskData = (maskData >= mask_thr).astype(np.float32)
         data = np.multiply(data,maskData)
 
     # if replace_nan, replace nan with zero
